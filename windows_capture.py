0d918d74da2cc288b97fb6f51f944dd09952a186
Limin <lm.fei@outlook.com>
2022-03-10 20:55:44 +0800

    draw lines

--- windows_capture.py
+++ windows_capture.py
@@ -15,6 +15,14 @@ from directkeys import PressKey, ReleaseKey, W, A, S, D, AUP, ADOWN, ALEFT, ARIG
 # print('up')
 # ReleaseKey(AUP)
 
+def draw_lines(img, lines):
+    try:
+        for line in lines:
+            coords = line[0]
+            cv2.line(img, (coords[0], coords[1]), (coords[2], coords[3]), [255, 255, 255], 3)
+    except:
+        pass
+
 def roi(img, vertices):
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -25,9 +33,19 @@ def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
 
+    # blur
+    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
+
     # vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]])
-    vertices = np.array([[132, 500], [132, 350], [300, 250], [500, 250], [655, 350], [655, 500]])
+    vertices = np.array([[132, 500], [132, 350], [300, 250], [500, 250], [655, 350], [655, 500]])   # Need for Speed 20 bumper view
+    
+    # cut the img
     processed_img = roi(processed_img, [vertices])
+
+    # draw lines use Hough algorithm
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, 20, 15)
+    draw_lines(processed_img, lines)
+
     return processed_img
 
 t0 = time.time()
